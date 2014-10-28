6d46a18fb7f251831a9d59c9b4ebf59b88b0617e
Ben <ben@Bens-MacBook-Pro-2.local>
2014-10-28 01:44:06 -0400

    test

--- php/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$db = new mysqli('localhost', 'lorantbe_public', 'public123', 'lorantbe_northwind');
-	
-	if($db->connect_errno > 0){
-	    die('Unable to connect to database [' . $db->connect_error . ']');
-	}
-?>
