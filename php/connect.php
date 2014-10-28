fd2a84772e5e3250c6e6b30464b0448745cabc01
Ben <BenLorantfy@users.noreply.github.com>
2014-10-28 01:29:38 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php
-	$db = new mysqli('localhost', 'root', 'root', 'northwind');
+	$db = new mysqli('localhost', 'lorantbe_public', 'public123', 'lorantbe_northwind');
 	
 	if($db->connect_errno > 0){
 	    die('Unable to connect to database [' . $db->connect_error . ']');
